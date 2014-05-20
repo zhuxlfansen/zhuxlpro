afe8badd4069d84f09e234aa50b2484d1c8f148b
zhuxlfansen <361796242@qq.com>
2014-05-20 18:06:49 +0800

    1

--- ahetyy/src/com/etyy/integration/duty/dao/DutyDao.java
+++ ahetyy/src/com/etyy/integration/duty/dao/DutyDao.java
@@ -1,10 +1,5 @@
 package com.etyy.integration.duty.dao;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-
-import org.apache.ibatis.annotations.Param;
 
 import com.etyy.common.integration.dao.annotation.DaoMaster;
 
